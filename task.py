7ade6c57a4e458c67bbce9f965b948111bab0b77
gonzalofernandez99 <fernandezgonzalo55@gmail.com>
2023-05-20 13:04:33 -0300

    Prueba 2

--- task.py
+++ task.py
@@ -1,6 +1,19 @@
 from RPA.Robocorp.WorkItems import WorkItems
+import os
+from RPA.Excel.Files import Files
+from RPA.Tables import Tables
 
 def minimal_task():
+    Excel = os.path.join('output', 'prueba.xlsx')
+    ws = Files()
+    tables = Tables()
+    headers = ["title", "date", "description", "name_file", "number_of_phrases", "contains_money"]
+    Data = ("title2", "d2ate", "desc2ription", "name_f2ile", "number_2of_phrases", "conta2ins_money")
+    
+    table = tables.create_table(data=Data, columns=headers)
+    ws.create_workbook(path=Excel,sheet_name="hoja1",fmt="xlsx")
+    ws.append_rows_to_worksheet(content=table,name="hoja1",header=True)
+    ws.save_workbook()
     
     wi = WorkItems()
     wi.get_input_work_item()
