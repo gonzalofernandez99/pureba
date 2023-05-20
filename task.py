11ceda4886988073bf639da4b3e52527a0cd06dc
gonzalofernandez99 <fernandezgonzalo55@gmail.com>
2023-05-20 13:23:43 -0300

    Segunda prueba 3

--- task.py
+++ task.py
@@ -4,9 +4,14 @@ from RPA.Excel.Files import Files
 from RPA.Tables import Tables
 
 def minimal_task():
-    Excel = os.path.join('output', 'prueba.xlsx')
+    
     ws = Files()
     tables = Tables()
+    artifacts_dir = os.path.join(os.getcwd(), 'output')
+    Excel = os.path.join(artifacts_dir, 'prueba.xlsx')
+    if not os.path.exists(artifacts_dir):
+        os.makedirs(artifacts_dir)
+        
     headers = ["title", "date", "description", "name_file", "number_of_phrases", "contains_money"]
     Data = ("title2", "d2ate", "desc2ription", "name_f2ile", "number_2of_phrases", "conta2ins_money")
     
@@ -19,6 +24,8 @@ def minimal_task():
     wi.get_input_work_item()
     customers = wi.get_work_item_variable("customers")
     print(customers)
-    wi.get_work_item_file(name='prueba.xlsx',path=Excel)
+    
+    
+    
 if __name__ == "__main__":
     minimal_task()
